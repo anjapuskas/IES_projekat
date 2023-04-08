204e2e4444fd70309d775fb3ca6786565d7d1a33
Anja Puskas <101398235+anjapuskas@users.noreply.github.com>
2023-04-08 19:24:51 +0200

    Add files via upload

---
+++ Model_labs/Common/Enums.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FTN.Common
+{
+    public enum UnitMultiplier : short
+    {
+        G = 1,
+        M = 2,
+        T = 3,
+        c = 4,
+        d = 5,
+        k = 6,
+        m = 7,
+        micro = 8,
+        n = 9,
+        none = 10,
+        p = 11,
+    }
+
+    public enum UnitSymbol : short
+    {
+        A = 1,
+        F = 2,
+        H = 3,
+        Hz = 4,
+        J = 5,
+        N = 6,
+        Pa = 7,
+        S = 8,
+        V = 9,
+        VA = 10,
+        VAh = 11,
+        VAr = 12,
+        VArh = 13,
+        W = 14,
+        Wh = 15,
+        deg = 16,
+        degC = 17,
+        g = 18,
+        h = 19,
+        m = 20,
+        m2 = 21,
+        m3 = 22,
+        min = 23,
+        none = 24,
+        ohm = 25,
+        rad = 26,
+        s = 27,
+    }
+
+
+
+}
